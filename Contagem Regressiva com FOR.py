48038c78f8fa8d5f1e12e2ddeaa05b3dd43b3542
Cristyan Silva <cristian.silvarosario@gmail.com>
2024-09-17 20:45:57 -0300

    Contagem Regressiva.
    Aprendendo a usar o FOR (loops).

---
+++ Contagem Regressiva com FOR.py
@@ -0,0 +1,8 @@
+from time import sleep
+print('Haverá uma grande contagem regressiva, esteja preparado!.')
+sleep(2)
+for i in range(10, -1, -1):
+    print(i)
+    sleep(1)
+print('BOOOM, EXPLODIU!.')
+    
