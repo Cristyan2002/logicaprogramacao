3d45b79cf045ca3698d2fda8ad4b764ce1c16318
Cristyan Silva <cristian.silvarosario@gmail.com>
2024-09-18 17:10:45 -0300

    Create Analise de Pessoas Utilizando FOR.py

---
+++ Analise de Pessoas Utilizando FOR.py
@@ -0,0 +1,11 @@
+calculo_idade = 0
+for i in range(1,5):
+    print(f'------{i}ª PESSOA------')
+    p = str(input('NOME: '))
+    idade = int(input('IDADE:'))
+    s = str(input('SEXO: M/F: '))
+    calculo_idade += idade
+    
+print(i)
+
+
