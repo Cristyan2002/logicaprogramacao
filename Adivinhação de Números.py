043d245b5275ab140cb909883f34b85c6aeca0bd
Cristyan Silva <cristian.silvarosario@gmail.com>
2024-09-19 23:37:25 -0300

    Adivinhação de número
    Utilizando WHILE.

--- Adivinhação de Números.py
+++ Adivinhação de Números.py
@@ -1,8 +1,13 @@
 from random import randint
 cpu = randint(1,10)
-usuario = int(input('Pensei em um número, dúvido você adivinhar!.'))
-
-while usuario < cpu:
-    print('Você não acertou, pensei em um número maior.')
-while usuario > cpu:
-    print('Você não acertou, pensei em um número menor.')
+tentativas = 0
+while True:
+    usuario = int(input('Pensei em um número, dúvido você adivinhar!.'))
+    tentativas += 1
+    if cpu == usuario:
+        print('Você acertou eu pensei no número {}'.format(cpu))
+        break
+    elif cpu > usuario:
+        print('Eu pensei em um número maior!')
+    elif cpu < usuario:
+        print('Eu pensei em um número menor!')
